3041a1a31a0ea3b7004d41cca0a27f00ff02b079
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-05-15 16:40:18 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.facebook.instantarticles',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="Plone plugin for Facebook's instant articles integration",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

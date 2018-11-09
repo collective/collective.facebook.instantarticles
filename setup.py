da4bb9b8e0f683e2ef3ac1b36e169ca99094b8ab
agitator <agitator@users.noreply.github.com>
2018-11-09 17:19:37 +0900

    imho this runs on plone 5
    @cekk ... at least it does for me ;-)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.3",
-        # "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",

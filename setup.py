0d5473e979f7914753f8c161afbb21c33b976224
Andrea Cecchi <andrea.cecchi85@gmail.com>
2018-11-10 06:33:32 +0100

    add 5.1 classifier

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",

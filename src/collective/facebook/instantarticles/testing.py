dd1a68ca0461e81e84309d3410548fa512ad9946
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-04-14 17:57:20 +0200

    remove robot

--- src/collective/facebook/instantarticles/testing.py
+++ src/collective/facebook/instantarticles/testing.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
-from plone.testing import z2
 
 import collective.facebook.instantarticles
 
@@ -38,14 +36,4 @@ COLLECTIVE_FACEBOOK_INSTANTARTICLES_INTEGRATION_TESTING = IntegrationTesting(
 COLLECTIVE_FACEBOOK_INSTANTARTICLES_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(COLLECTIVE_FACEBOOK_INSTANTARTICLES_FIXTURE,),
     name='CollectiveFacebookInstantarticlesLayer:FunctionalTesting',
-)
-
-
-COLLECTIVE_FACEBOOK_INSTANTARTICLES_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        COLLECTIVE_FACEBOOK_INSTANTARTICLES_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name='CollectiveFacebookInstantarticlesLayer:AcceptanceTesting',
 )
